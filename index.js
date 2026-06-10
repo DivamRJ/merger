bc0530eb6ae292c21ed1f072c830007e862e8f13
Divam Rao Joshi <divamraojoshii@gmail.com>
2026-06-11 03:39:32 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const port = 3000;
+console.log("another bullshit try")
 const app = express();
 console.log("hello from the changed merged etc")
 app.get('/', (req, res) => {
